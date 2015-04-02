79343884e869189afa343a51934772b0abdd5d83
Freek Wiekmeijer <freek.wiekmeijer@ldi-systems.com>
2015-04-02 16:43:01 +0200

    Encoder

---
+++ flexrle/common.py
@@ -0,0 +1,5 @@
+from struct import calcsize
+
+
+preamble_fmt = ">H"
+preamble_size = calcsize(preamble_fmt)
--- flexrle/encode.py
+++ flexrle/encode.py
@@ -1,3 +1,32 @@
-def encode():
+from operator import itemgetter
+from struct import pack
+
+from common import preamble_fmt
+
+
+def get_word(buf, pos, size):
+    return (pos+size <= len(buf)) and buf[pos:pos+size] or None
+
+def count_head(buf, pos, w):
+    if not w:
+        return 0
+    initial_pos = pos
+    size = len(w)
+    while buf[pos:pos+size] == w:
+        pos += size
+    return pos-initial_pos
+
+def encode(buf):
     """Apply FlexRLE runlength encoding"""
-    pass
+    sizes = {x: 2**x for x in range(4)}
+    out = ''
+    pos = 0
+    while pos < len(buf):
+        words = {b: get_word(buf, pos, s) for (b,s) in sizes.items()}
+        head = {b: count_head(buf, pos, words[b]) for b in sizes}
+        (code, jmp) = max(head.items(), key=itemgetter(1))
+        word_size = sizes[code]
+        out += pack(preamble_fmt, (code << 13) | ((jmp & 0xFFFF) >> code))
+        out += buf[pos:word_size]
+        pos += jmp
+    return out
